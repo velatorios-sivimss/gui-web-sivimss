e31a6bd7abd895188cb33e8271113ad5db237f83
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-03-02 12:51:01 -0600

    añadidos servicios y models

--- projects/sivimss-gui/src/app/modules/inventario-vehicular/inventario-vehicular.module.ts
+++ projects/sivimss-gui/src/app/modules/inventario-vehicular/inventario-vehicular.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InventarioVehicularRoutingModule } from './inventario-vehicular-routing.module';
+import { InventarioVehicularService } from './services/inventario-vehicular.service';
 
 
 
@@ -9,6 +10,7 @@ import { InventarioVehicularRoutingModule } from './inventario-vehicular-routing
   imports: [
     CommonModule,
     InventarioVehicularRoutingModule
-  ]
+  ],
+  providers: [InventarioVehicularService]
 })
 export class InventarioVehicularModule { }
---
+++ projects/sivimss-gui/src/app/modules/inventario-vehicular/models/vehiculo.interface.ts
@@ -0,0 +1,8 @@
+export interface Vehiculo {
+    id: number,
+    velatorio: string,
+    uso: string,
+    responsable: string,
+    tipoVehiculo: string,
+    estatus: boolean
+}
