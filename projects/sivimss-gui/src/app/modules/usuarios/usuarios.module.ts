a002b1d1f7bcd02be588fa9679aa56ef9d2e3962
Alberto Marin <alberto.marind@gmail.com>
2023-03-10 02:16:03 -0600

    Se crean dos directivas para manejar la ultima celda fija en las tablas

--- projects/sivimss-gui/src/app/modules/usuarios/usuarios.module.ts
+++ projects/sivimss-gui/src/app/modules/usuarios/usuarios.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { CeldaStickyModule } from "projects/sivimss-gui/src/app/shared/celda-sticky/celda-sticky.module";
 
 import {UsuariosRoutingModule} from './usuarios-routing.module';
 import {UsuariosComponent} from './components/usuarios/usuarios.component';
@@ -33,7 +34,8 @@ import {TablePanelModule} from "../../shared/table-panel/table-panel.module";
     CalendarModule,
     OverlayPanelModule,
     OverlayPanelOpcionesModule,
-    TablePanelModule
+    TablePanelModule,
+    CeldaStickyModule
   ],
   providers: [
     UsuarioService
