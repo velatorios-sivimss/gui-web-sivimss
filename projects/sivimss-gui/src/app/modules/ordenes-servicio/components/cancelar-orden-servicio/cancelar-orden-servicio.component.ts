1c9b07efae3efd83b4a737237db3da783a10e2c9
Alberto Marin <alberto.marind@gmail.com>
2023-03-15 09:43:00 -0600

    Se crean estilos para componente accordion de primeng, se crea componente cancelacion orden de servicio, se agregan mas iconos

---
+++ projects/sivimss-gui/src/app/modules/ordenes-servicio/components/cancelar-orden-servicio/cancelar-orden-servicio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cancelar-orden-servicio',
+  templateUrl: './cancelar-orden-servicio.component.html',
+  styleUrls: ['./cancelar-orden-servicio.component.scss']
+})
+export class CancelarOrdenServicioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
