1ae8fbb28a9413f0d6a0564fa5a84c7761eeaebe
GRUPOPM\mparedes <martin.paredes@people-media.com.mx>
2023-04-10 19:18:21 -0600

    Ajustes de maquetacion para modulo de pagos

---
+++ projects/sivimss-gui/src/app/modules/pagos/generar-recibo-pago/services/generar-recibo-pago.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpRespuesta } from 'projects/sivimss-gui/src/app/models/http-respuesta.interface';
+import { BaseService } from 'projects/sivimss-gui/src/app/utils/base-service';
+import { environment } from 'projects/sivimss-gui/src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class GenerarReciboService extends BaseService<HttpRespuesta<any>, any> {
+
+  private auth_token2: string = "eyJzaXN0ZW1hIjoic2l2aW1zcyIsImFsZyI6IkhTMjU2In0.eyJzdWIiOiJ7XCJpZFZlbGF0b3Jpb1wiOlwiMVwiLFwiaWRSb2xcIjpcIjFcIixcImRlc1JvbFwiOlwiQ09PUkRJTkFET1IgREUgQ0VOVFJcIixcImlkRGVsZWdhY2lvblwiOlwiMVwiLFwiaWRPZmljaW5hXCI6XCIxXCIsXCJpZFVzdWFyaW9cIjpcIjFcIixcImN2ZVVzdWFyaW9cIjpcIjFcIixcImN2ZU1hdHJpY3VsYVwiOlwiMVwiLFwibm9tYnJlXCI6XCIxIDEgMVwiLFwiY3VycFwiOlwiMVwifSIsImlhdCI6MTY3OTY4NzM1NSwiZXhwIjoxNjgwMjkyMTU1fQ.Ah2L-rpfJTpsu8VHhb4OxOe_Nj7cUxI_bB9XjAfAy2Y";
+  constructor(protected _http: HttpClient) {
+    super(_http, `${environment.api.mssivimss}`, "", "", 0, "", "", "");
+  }
+}
