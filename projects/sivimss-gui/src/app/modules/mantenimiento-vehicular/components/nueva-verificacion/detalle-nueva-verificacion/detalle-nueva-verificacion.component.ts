a240bfaea83c4f3f96746435462be79b4ea6df72
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-04-26 21:47:40 -0600

    Detalle Nueva Verificacion

--- projects/sivimss-gui/src/app/modules/mantenimiento-vehicular/components/nueva-verificacion/detalle-nueva-verificacion/detalle-nueva-verificacion.component.ts
+++ projects/sivimss-gui/src/app/modules/mantenimiento-vehicular/components/nueva-verificacion/detalle-nueva-verificacion/detalle-nueva-verificacion.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {tablaRin} from "../../../constants/tabla-rines";
 
 @Component({
   selector: 'app-detalle-nueva-verificacion',
@@ -7,12 +8,13 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DetalleNuevaVerificacionComponent implements OnInit {
 
-  data = [{rin: 38, precision: 38}, {rin: 38, precision: 38}, {rin: 38, precision: 38}, {rin: 38, precision: 38}];
+  data = tablaRin;
 
   constructor() {
   }
 
   ngOnInit(): void {
+
   }
 
 }
